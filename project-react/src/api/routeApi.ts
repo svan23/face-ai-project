4b6478980710d01ef2e4ec02431a39c526576ee8
svan23 <vansla>
2025-04-07 01:04:59 -0700

    https

--- project-react/src/api/routeApi.ts
+++ project-react/src/api/routeApi.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Always use the full URL
-const API_BASE_URL = "http://128.85.34.139"; 
+const API_BASE_URL = "https://128.85.34.139"; 
 
 export const getBestMatch = async (file: File) => {
   const formData = new FormData();
